0cb5e48cc25b36afebd0e6d9886ea77cfbcf7712
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-04-20 23:19:31 +0530

    Add files via upload

---
+++ TelCoColorCoderTest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "TelCoColorCoderLogic.hpp"
+#include "TelCoColorCoderTestingScript.cpp"
+
+int main() {
+    testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
+    testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
+
+    testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
+    testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
+
+    return 0;
+}
