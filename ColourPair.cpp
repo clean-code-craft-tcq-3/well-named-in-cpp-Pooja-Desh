0b987a6c113c25ce8d580e47fa0b7a5f373c62eb
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-06-10 14:31:56 +0530

    Update ColourPair.cpp

--- ColourPair.cpp
+++ ColourPair.cpp
@@ -1,6 +1,11 @@
 #include "TelCoColorCoderLogic.hpp"
 using namespace TelCoColorCoder;
 
+    const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
     ColorPair GetColorFromPairNumber(int pairNumber) 
     {
         int zeroBasedPairNumber = pairNumber - 1;
